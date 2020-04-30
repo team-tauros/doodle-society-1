a18fc6b1e296b0c8dc6901da5b3d036325e3257e
James Easter <james.m.easter@gmail.com>
2020-04-29 23:30:35 -0500

    (cleanup) removed controls, added styling

--- src/Map/SmallMap.js
+++ src/Map/SmallMap.js
@@ -1,25 +1,33 @@
 import React, { useState } from 'react'
 import { GoogleMap, Marker, useLoadScript } from '@react-google-maps/api'
 
-const SmallMap = () => {
+const SmallMap = ({ setCoords }) => {
   const [mapRef, setMapRef] = useState(null);
   const [center, setCenter] = useState({ lat: 29.971065, lng: -90.101533 });
-  const [coords, setCoords] = useState(center);
-
 
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_KEY
   });
 
+  const options = {
+      zoomControl: true,
+      fullscreenControl: true,
+      mapTypeControl: false,
+      scaleControl: false,
+      streetViewControl: false,
+    }
+
   const renderMap = () => {
     return (
       <GoogleMap
         id='example-map'
+        options={options}
         mapContainerStyle={{
-          height: "43vh",
-          width: "28vw",
-          "margin-top": "20px",
-          "margin-bottom": "20px",
+          height: "44vh",
+          width: "29vw",
+          "marginBottom": "20px",
+          border: "solid blue 4px",
+          "borderRadius": "10px",
         }}
         zoom={11}
         center={center}
