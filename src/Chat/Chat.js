f2c3d4bf2ee5760da9bfeb27a6fa408886e62706
Heather Haylett <heatherhaylett@gmail.com>
2020-04-29 19:42:10 -0500

    (feat) implement pusher realtime messaging

--- src/Chat/Chat.js
+++ src/Chat/Chat.js
@@ -1,9 +1,59 @@
-import React from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
+import Pusher from 'pusher-js';
+import ChatList from './ChatList';
+import ChatBox from './ChatBox';
 
-const Chat = () => {
-  return (
-    <h1>Chat page</h1>
-  )
+class Chat extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: '',
+      username: '',
+      chats: []
+    };
+  }
+
+  componentDidMount() {
+    const username = window.prompt('Username: ', 'Anonymous');
+    this.setState({ username });
+    const pusher = new Pusher('5f62025ddf3789ba4978', {
+      cluster: 'us2',
+    });
+    const channel = pusher.subscribe('chat');
+    channel.bind('message', data => {
+      this.setState({ chats: [...this.state.chats, data], test: '' });
+    });
+    this.handleTextChange = this.handleTextChange.bind(this);
+  }
+
+  handleTextChange(e) {
+    if (e.keyCode === 13) {
+      const payload = {
+        username: this.state.username,
+        message: this.state.text
+      };
+      axios.post('http://localhost:4000/message', payload);
+    } else {
+      this.setState({ text: e.target.value });
+    }
+  }
+
+  render() {
+    return (
+      <div>
+          <h1>Welcome to React-Pusher Chat</h1>
+        <section>
+          <ChatList chats={this.state.chats} />
+          <ChatBox
+            text={this.state.text}
+            username={this.state.username}
+            handleTextChange={this.handleTextChange}
+          />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Chat;
